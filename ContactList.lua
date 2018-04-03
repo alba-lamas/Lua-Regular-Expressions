b03c575b1b332923a8d3fecf9336d78be245446c
Alba Lamas <alba.v.lmas@gmail.com>
2018-04-03 18:51:24 +0200

    Little example

--- ContactList.lua
+++ ContactList.lua
@@ -6,3 +6,32 @@
 -- To change this template use File | Settings | File Templates.
 --
 
+
+function file_exists(file)
+    local f = io.open(file, "rb")
+    if f then f:close() end
+    return f ~= nil
+end
+
+-- get all lines from a file, returns an empty
+-- list/table if the file does not exist
+function lines_from(file)
+    if not file_exists(file) then return {} end
+    lines = {}
+    for line in io.lines(file) do
+        lines[#lines + 1] = line
+    end
+    return lines
+end
+
+-- tests the functions above
+local file = arg[1]
+local lines = lines_from(file)
+
+-- print all line numbers and their contents
+for k,v in pairs(lines) do
+    matched = false
+    if not string.match(v, 'Nom:[A-Z]+[a-z]*') == nil then
+        print("OK")
+    end
+end
