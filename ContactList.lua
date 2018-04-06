fc3da6a919b99b81fc541aa2e0efd60aa6578422
Jordi Ricard Onrubia Palacio <jordirop@gmail.com>
2018-04-06 15:44:07 +0200

    Code Clean

--- ContactList.lua
+++ ContactList.lua
@@ -1,35 +1,18 @@
+function doThing(ifile)
+    local rifile = io.open(file, "r")
 
-function file_exists(file)
-    local f = io.open(file, "rb")
-    if f then f:close() end
-    return f ~= nil
-end
-
--- get all lines from a file, returns an empty
--- list/table if the file does not exist
-function lines_from(file)
-    if not file_exists(file) then return {} end
-    lines = {}
-    for line in io.lines(file) do
-        lines[#lines + 1] = line
-    end
-    return lines
-end
-
--- tests the functions above
-local file = "test.txt"
-local lines = lines_from(file)
-
--- print all line numbers and their contents
-for k,v in pairs(lines) do
-    matched = false
-    if string.match(v, 'Nom: %u%l*%s%u%l*%s%u%l*') ~= nil then
-        print("OK-->"..v)
-    elseif string.match(v, 'email: %l+@%l+%.%l+') ~= nil then
-        print("OK-->"..v)
-    elseif string.match(v, 'Telf: %d9') ~= nil then
-        print("OK-->"..v)
-    else
-        print("FATAL-->"..v)
+    for line in rifile:lines() do
+        if string.match(line, 'Nom: %u%l*%s%u%l*%s%u%l*') ~= nil then
+            print("OK-->"..line)
+        elseif string.match(line, 'email: %l+@%l+%.%l+') ~= nil then
+            print("OK-->"..line)
+        elseif string.match(line, 'Telf: %d9') ~= nil then
+            print("OK-->"..line)
+        else
+            print("FATAL-->"..line)
+        end
     end
 end
+
+file = "test.txt"
+doThing()
